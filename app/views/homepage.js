fc8ee6b5ea8984e14791d464ff0f66a148896b8e
Lukasz Sobel <lukasz.sobel@hotmail.com>
2020-10-13 21:56:57 +0200

    add state for aplication

---
+++ app/views/homepage.js
@@ -0,0 +1,17 @@
+const React = require('react');
+// import React from 'react';
+// import styles from './Homepage.module.scss';
+
+class Homepage extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
+                <p>This app will help you track your time and inform you when it's time to rest.</p>
+            </div>
+        );
+    }
+};
+
+// export default Homepage;
+module.exports = Homepage; 
